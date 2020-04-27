d00a64aa1565d3d87abb8c8f35ee3d88ae47fb0c
Section9-Togusa <corbindac@gmail.com>
2020-04-27 08:57:37 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const hour = parseInt(string, 10)
+  if (hour < 12) return "Good Morning"
+  if (hour >= 12 && hour <= 15) return "Good Afternoon"
+  return "Good Evening"
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(TEST) {
+document.getElementById('greeting').innerText = "TEST"; 
+}
